5f4ee0a90ac0755d1f41ce1bae513091c296ca26
Артем <loktevartem880@gmail.com>
2025-09-25 01:24:34 +0300

    Первая версия практической

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Считываем число с клавиатуры
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+        double x;
+
+        // Проверяем, что пользователь ввёл число
+        if (!double.TryParse(input, out x))
+        {
+            Console.WriteLine("Ошибка: введено не число!");
+            return;
+        }
+
+      
+        double result = (Math.Sin(x) + Math.Cos(x)) / 2;
+
+       
+        Console.WriteLine($"Результат: {result}");
+    }
+}
